f0f220ab7d4bbfb4de0d06b5b1ed2e6c4673ab0b
Ewan Stott <ewan.stott1@gmail.com>
2024-03-09 07:32:22 +0000

    log out added

--- src/components/account/PatientDashboard.jsx
+++ src/components/account/PatientDashboard.jsx
@@ -1,7 +1,22 @@
+import { useDispatch } from "react-redux";
+import { setLoggedIn } from "../../redux/accountSlice";
+import { useNavigate } from "react-router-dom";
+
 const PatientDashboard = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   return (
     <>
       <h1>PatientDashboard</h1>
+      <button
+        onClick={() => {
+          dispatch(setLoggedIn(false)); //updates logged in status
+          navigate("/"); //naviages back to home page
+        }}
+      >
+        Logout
+      </button>
     </>
   );
 };
--- src/components/account/PractionerDashboard.jsx
+++ src/components/account/PractionerDashboard.jsx
@@ -1,9 +1,23 @@
-const practionionerDashboard = () => {
+import { useDispatch } from "react-redux";
+import { setLoggedIn } from "../../redux/accountSlice";
+import { useNavigate } from "react-router-dom";
+
+const PractionionerDashboard = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   return (
     <>
       <h1>Practionioner Dashboard</h1>
+      <button
+        onClick={() => {
+          dispatch(setLoggedIn(false)); //updates logged in status
+          navigate("/"); //naviages back to home page
+        }}
+      >
+        Logout
+      </button>
     </>
   );
 };
 
-export default practionionerDashboard;
+export default PractionionerDashboard;
