93e7ecd848d35d1f3dd7462f25828d61df2ebb19
Ewan Stott <ewstott@expediagroup.com>
2024-04-25 08:45:28 +0100

    message sends successfully from practitionerDetails

--- src/components/message/MessageInput.jsx
+++ src/components/message/MessageInput.jsx
@@ -1,18 +1,41 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { sendMessage } from "../../redux/messageSlice";
 import MainButton from "../MainButton";
 import { selectCurrentUser } from "../../redux/accountSlice";
+import { setNotification } from "../../redux/practitionerSlice";
+import axios from "axios";
 
-const MessageInput = ({ senderType, sender }) => {
+const MessageInput = ({ practitionerId, sender, senderType }) => {
   const [message, setMessage] = useState("");
+  const dispatch = useDispatch();
+
+  // const handleMessageSubmit = async (e) => {
+  //   e.preventDefault();
+  //   // Send message to backend
+  //   const response = await sendMessage({ senderType, sender, message });
+  //   // Handle response from backend if needed
+  //   setMessage(""); // Clear message input
+  // };
 
   const handleMessageSubmit = async (e) => {
     e.preventDefault();
-    // Send message to backend
-    const response = await sendMessage({ senderType, sender, message });
-    // Handle response from backend if needed
-    setMessage(""); // Clear message input
+    console.log("Send msg button clicked from practitionerDetails");
+    try {
+      const response = await axios.post(
+        `http://localhost:6001/message/add`,
+        { receiver_id: practitionerId, message: message },
+        {
+          headers: { token: localStorage.getItem("token") },
+        }
+      );
+      if (response.data.status) {
+        console.log("Message sent successfully:", response.data); // Log the response
+        dispatch(setNotification("Message sent"));
+        setMessage("");
+      }
+    } catch (error) {
+      console.error("Error sending message:", error);
+    }
   };
 
   return (
